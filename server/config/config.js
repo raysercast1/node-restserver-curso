7c94b9414cb7b24b47fb4523df78680690e528a2
Rayser <rayser.alejandro@gmail.com>
2019-10-30 19:13:53 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://raysercast:mVpu8XJMEgCXX4js@cluster0-p13nu.mongodb.net/test'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
